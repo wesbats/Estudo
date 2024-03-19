24410b8edeb1b6c344274ec780fe2f5d780cc140
Weslley Batista <weslleyvitorbatista@gmail.com>
2024-03-19 03:25:12 -0300

    atualizado atribuicao do som para lista automatizada

--- js/aluramidi/main.js
+++ js/aluramidi/main.js
@@ -1,26 +1,15 @@
 // Teclas
-botaoPom = document.querySelector(".tecla_pom");
-botaoClap = document.querySelector(".tecla_clap");
-botaoTim = document.querySelector(".tecla_tim");
-botaoPuff = document.querySelector(".tecla_puff");
-botaoSplash = document.querySelector(".tecla_splash");
-botaoToim = document.querySelector(".tecla_toim");
-botaoPsh = document.querySelector(".tecla_psh");
-botaoTic = document.querySelector(".tecla_tic");
-botaoTom = document.querySelector(".tecla_tom");
+const listaTeclas = document.querySelectorAll(".tecla");
 
 // Atribiu funções
-botaoPom.onclick = () => tocaSom("pom");
-botaoClap.onclick = () => tocaSom("clap");
-botaoTim.onclick = () => tocaSom("tim");
-botaoPuff.onclick = () => tocaSom("puff");
-botaoSplash.onclick = () => tocaSom("splash");
-botaoToim.onclick = () => tocaSom("toim");
-botaoPsh.onclick = () => tocaSom("psh");
-botaoTic.onclick = () => tocaSom("tic");
-botaoTom.onclick = () => tocaSom("tom");
+listaTeclas.forEach(atribuiSom);
 
 // Funções
+function atribuiSom(botao) {
+  let som = botao.innerHTML.toLowerCase();
+  botao.onclick = () => tocaSom(som);
+}
+
 function tocaSom(som) {
   idPlayer = "#som_tecla_" + som;
   player = document.querySelector(idPlayer);
